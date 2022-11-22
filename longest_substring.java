753e1422b3f1b768246458879e57ce5ac5ff592a
Vidushi071 <114132172+Vidushi071@users.noreply.github.com>
2022-11-22 22:32:46 +0530

    Add files via upload

---
+++ longest_substring.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+//longest substring without repeating character
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left =0;
+        int maxcount = 0;
+      
+        Set<Character> set = new HashSet<>();
+        for(int right = 0; right < s.length(); right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left++));    
+            }
+            set.add(s.charAt(right));
+            maxcount = Math.max(maxcount, right - left +1);
+        }
+    return maxcount;
+    }
+}
